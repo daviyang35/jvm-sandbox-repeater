e3edbf78b9a79269fca627e9a4d544efc85509d0
daviyang35 <daviyang35@gmail.com>
2021-11-24 20:23:18 +0800

    打包包含Git信息 Sandbox 使用修改1.5.0版本

--- bin/package.sh
+++ bin/package.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+BIN_DIR=`dirname $0`
+PROJECT_ROOT_DIR=${BIN_DIR}/..
+
 # repeater's target dir
-BUILD_OUTPUT_DIR=../dist
+BUILD_OUTPUT_DIR=${BIN_DIR}/../dist
 REPEATER_TARGET_DIR=${BUILD_OUTPUT_DIR}/sandbox-module
 
 # exit shell with err_code
@@ -19,27 +22,27 @@ mkdir -p ${REPEATER_TARGET_DIR}/plugins
 mkdir -p ${REPEATER_TARGET_DIR}/cfg
 
 # Repeater Backend Service
-cp ../repeater-console/repeater-console-bootstrap/target/repeater-console-bootstrap.jar ${BUILD_OUTPUT_DIR}/repeater-console-bootstrap.jar
+cp ${PROJECT_ROOT_DIR}/repeater-console/repeater-console-bootstrap/target/repeater-console-bootstrap.jar ${BUILD_OUTPUT_DIR}/repeater-console-bootstrap.jar
 
 # Repeater Module Config
-cp ./cfg/repeater-logback.xml ${REPEATER_TARGET_DIR}/cfg/repeater-logback.xml
-cp ./cfg/repeater.properties ${REPEATER_TARGET_DIR}/cfg/repeater.properties
+cp ${BIN_DIR}/cfg/repeater-logback.xml ${REPEATER_TARGET_DIR}/cfg/repeater-logback.xml
+cp ${BIN_DIR}/cfg/repeater.properties ${REPEATER_TARGET_DIR}/cfg/repeater.properties
 
 # Repeater Module Manager
-cp ../repeater-agent/repeater-module/target/repeater-module-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/repeater-module.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-module/target/repeater-module-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/repeater-module.jar
 
 # Repeater Module Plugins
-cp ../repeater-agent/repeater-plugins/redis-plugin/target/redis-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/redis-plugin.jar
-cp ../repeater-agent/repeater-plugins/apache-http-client-plugin/target/apache-http-client-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/apache-http-client-plugin.jar
-cp ../repeater-agent/repeater-plugins/http-plugin/target/http-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/http-plugin.jar
-cp ../repeater-agent/repeater-plugins/java-plugin/target/java-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/java-plugin.jar
-cp ../repeater-agent/repeater-plugins/eh-cache-plugin/target/eh-cache-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/eh-cache-plugin.jar
-cp ../repeater-agent/repeater-plugins/ibatis-plugin/target/ibatis-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/ibatis-plugin.jar
-cp ../repeater-agent/repeater-plugins/caffeine-cache-plugin/target/caffeine-cache-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/caffeine-cache-plugin.jar
-cp ../repeater-agent/repeater-plugins/guava-cache-plugin/target/guava-cache-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/guava-cache-plugin.jar
-cp ../repeater-agent/repeater-plugins/socketio-plugin/target/socketio-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/socketio-plugin.jar
-cp ../repeater-agent/repeater-plugins/jpa-plugin/target/jpa-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/jpa-plugin.jar
-cp ../repeater-agent/repeater-plugins/hibernate-plugin/target/hibernate-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/hibernate-plugin.jar
-cp ../repeater-agent/repeater-plugins/mybatis-plugin/target/mybatis-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/mybatis-plugin.jar
-cp ../repeater-agent/repeater-plugins/okhttp-plugin/target/okhttp-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/okhttp-plugin.jar
-cp ../repeater-agent/repeater-plugins/dubbo-plugin/target/dubbo-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/dubbo-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/redis-plugin/target/redis-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/redis-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/apache-http-client-plugin/target/apache-http-client-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/apache-http-client-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/http-plugin/target/http-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/http-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/java-plugin/target/java-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/java-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/eh-cache-plugin/target/eh-cache-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/eh-cache-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/ibatis-plugin/target/ibatis-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/ibatis-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/caffeine-cache-plugin/target/caffeine-cache-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/caffeine-cache-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/guava-cache-plugin/target/guava-cache-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/guava-cache-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/socketio-plugin/target/socketio-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/socketio-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/jpa-plugin/target/jpa-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/jpa-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/hibernate-plugin/target/hibernate-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/hibernate-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/mybatis-plugin/target/mybatis-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/mybatis-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/okhttp-plugin/target/okhttp-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/okhttp-plugin.jar
+cp ${PROJECT_ROOT_DIR}/repeater-agent/repeater-plugins/dubbo-plugin/target/dubbo-plugin-*-jar-with-dependencies.jar ${REPEATER_TARGET_DIR}/plugins/dubbo-plugin.jar
--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile.console -t jvm-sandbox-console .
-docker build -f Dockerfile.agent -t jvm-sandbox-agent .
+# exit shell with err_code
+# $1 : err_code
+# $2 : err_msg
+exit_on_err()
+{
+    [[ ! -z "${2}" ]] && echo "${2}" 1>&2
+    exit ${1}
+}
+
+# build
+mvn clean package -Dmaven.test.skip=true || exit_on_err 1 "package repeater failed."
+
+# copy files
+sh bin/package.sh || exit_on_err 1 "copy repeater failed."
+
+docker build -f Dockerfile.console -t jvm-sandbox-console . || exit_on_err 1 "package repeater failed."
+docker build -f Dockerfile.agent -t openjdk:8-jdk-repeater . || exit_on_err 1 "package repeater failed."
